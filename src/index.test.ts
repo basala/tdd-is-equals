fabb14acf6cc1fd0b5a3dc5c27661eaf55ff105e
kira <kira@fanruan.com>
2023-08-01 15:59:39 +0800

    feat: tdd实践 #none

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,37 @@
 import { isEquals } from ".";
 
-it("测试用例", () => {
-  expect(isEquals(1, 1)).toBe(true);
+describe("测试用例", () => {
+  test("类型不相等的情况", () => {
+    expect(isEquals(1, "1")).toBe(false);
+    expect(isEquals(true, "true")).toBe(false);
+  });
+
+  test("基本类型", () => {
+    expect(isEquals(1, 2)).toBe(false);
+    expect(isEquals(1, 1)).toBe(true);
+    expect(isEquals(true, true)).toBe(true);
+    expect(isEquals("1", "1")).toBe(true);
+    expect(isEquals(null, null)).toBe(true);
+    expect(isEquals(undefined, undefined)).toBe(true);
+    expect(isEquals(undefined, null)).toBe(false);
+    expect(isEquals(Symbol(1), Symbol(1))).toBe(false);
+  });
+
+  test("NaN", () => {
+    expect(isEquals(NaN, NaN)).toBe(true);
+  });
+
+  test("-0 +0", () => {
+    expect(isEquals(-0, +0)).toBe(true);
+  });
+
+  test("对象", () => {
+    expect(isEquals({}, {})).toBe(true);
+    expect(isEquals({ a: 1 }, { a: 1 })).toBe(true);
+    expect(isEquals({ a: 1 }, { a: 2 })).toBe(false);
+    expect(isEquals({ a: null }, { a: null })).toBe(true);
+    expect(isEquals({ a: null }, { a: 1 })).toBe(false);
+    expect(isEquals({ a: 1 }, { a: 1, b: 2 })).toBe(false);
+    expect(isEquals({ a: { b: 1 } }, { a: { b: 1 } })).toBe(true);
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,26 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function isEquals(a: any, b: any): boolean {
+export function isEquals(a: unknown, b: unknown): boolean {
+  if (typeof a !== typeof b) {
+    return false;
+  }
+
+  if (Number.isNaN(a) && Number.isNaN(b)) {
+    return true;
+  }
+
+  if (typeof a === "object" && typeof b === "object") {
+    if (a === null || b === null) {
+      return a === b;
+    }
+
+    const keysA = Object.keys(a);
+    const keysB = Object.keys(b);
+
+    if (keysA.length !== keysB.length) {
+      return false;
+    }
+
+    return keysA.every((key) => isEquals((a as any)[key], (b as any)[key]));
+  }
+
   return a === b;
 }
